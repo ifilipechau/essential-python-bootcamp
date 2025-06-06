a00c6a4d11091a5d2ced730d695bc8a8cdd29f22
Filipe Chau <filipechau.dev@gmail.com>
2025-06-06 19:57:53 +0200

    Add exercise for addition of two numbers

--- day1_basics/exercises.py
+++ day1_basics/exercises.py
@@ -15,3 +15,17 @@ idade = input("Digite sua idade: ")
 cidade = input("Digite sua cidade: ")
 
 print(f"Olá, eu sou {nome}, tenho {idade} anos e vivo em {cidade}.")
+
+# 3. Peca ao usuário para digitar dois números e imprima a soma deles.
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é {soma}.")
+
+# 4. Crie uma string "Python é incrível!" e imprima apenas a palavra "incrível".
+
+frase = "Python é incrível!"
+palavra = frase.split()[2]
+
+print(f"{palavra}")
